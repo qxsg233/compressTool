2a825ce9927090c72a642f59376bf4fd84943a87
qbw <qbw1667226570@163.com>
2022-12-07 17:10:35 +0800

    对主界面添加了按键和菜单

--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -6,9 +6,51 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //实例化对象
+    QMenu *pMenuOrder = new QMenu(tr("菜单"), this);
+    QAction *pActionPassword = new QAction(tr("密码"), this);
+    QAction *pActionSet = new QAction(tr("设置"), this);
+    QAction *pActionAbout = new QAction(tr("关于"), this);
+    ui->menuBar->addMenu(pMenuOrder);
+    ui->menuBar->addAction(pActionAbout);
+    pMenuOrder->addAction(pActionPassword);
+    pMenuOrder->addAction(pActionSet);
+
+    //连接槽函数
+    connect(pActionPassword, SIGNAL(triggered(bool)), this, SLOT(slot_actionPassword_triggered(bool)));
+    connect(pActionSet, SIGNAL(triggered(bool)), this, SLOT(slot_actionSet_triggered(bool)));
+    connect(pActionAbout, SIGNAL(triggered(bool)), this, SLOT(slot_actionAbout_triggered(bool)));
+    connect(ui->pushButton_compress, SIGNAL(clicked(bool)), this, SLOT(slot_pushButton_compress_clicked(bool)));
+    connect(ui->pushButton_uncompress, SIGNAL(clicked(bool)), this, SLOT(slot_pushButton_uncompress_clicked(bool)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::slot_actionPassword_triggered(bool)
+{
+    qDebug()<<"slot_actionPassword_triggered";
+}
+
+void MainWindow::slot_actionSet_triggered(bool)
+{
+    qDebug()<<"slot_actionSet_triggered";
+}
+
+void MainWindow::slot_actionAbout_triggered(bool)
+{
+    qDebug()<<"slot_actionAbout_triggered";
+}
+
+void MainWindow::slot_pushButton_compress_clicked(bool)
+{
+    qDebug()<<"slot_pushButton_compress_clicked";
+}
+
+void MainWindow::slot_pushButton_uncompress_clicked(bool)
+{
+    qDebug()<<"slot_pushButton_uncompress_clicked";
 }
--- source/mainwindow.h
+++ source/mainwindow.h
@@ -2,7 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QMenu>
+#include <QAction>
+#include <QDebug>
 namespace Ui {
 class MainWindow;
 }
@@ -15,8 +17,18 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void slot_actionPassword_triggered(bool);       //密码活动点击事件槽函数
+    void slot_actionSet_triggered(bool);            //设置活动点击事件槽函数
+    void slot_actionAbout_triggered(bool);          //关于活动点击事件槽函数
+    void slot_pushButton_compress_clicked(bool);    //压缩按键点击事件槽函数
+    void slot_pushButton_uncompress_clicked(bool);  //解压按键点击事件槽函数
 private:
     Ui::MainWindow *ui;
+    QMenu *pMenuOrder;          //菜单对象指针
+    QAction *pActionPassword;   //密码活动对象指针
+    QAction *pActionSet;        //设置活动对象指针
+    QAction *pActionAbout;      //关于活动对象指针
 };
 
 #endif // MAINWINDOW_H
