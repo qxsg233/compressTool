e5ed4e9d32357f6e24ce07cf9f775f2188d3d972
qbw <qbw1667226570@163.com>
2022-12-08 18:22:27 +0800

    重现l写istwidget类

--- source/widget/mylistwidget.cpp
+++ source/widget/mylistwidget.cpp
@@ -1,7 +1,85 @@
 #include "mylistwidget.h"
 
-MyListWidget::MyListWidget(QListWidget *parent) : QListWidget(parent)
+MyListWidget::MyListWidget(QWidget *parent) : QListWidget(parent)
 {
-
+    this->setAcceptDrops(true);
+    this->clear();
+    m_list.clear();
 }
 
+void MyListWidget::addItem(QString str)
+{
+    qDebug()<<"addItem";
+    if(str.isEmpty())
+    {
+        return;
+    }
+    if(m_list.indexOf(str)<0)
+    {
+        m_list.append(str);
+        this->clear();
+        this->addItems(m_list);
+    }
+}
+
+void MyListWidget::addItems(QStringList list)
+{
+    qDebug()<<"addItems";
+    if(list.isEmpty())
+    {
+        return;
+    }
+
+    foreach (QString temp, list) {
+        if(m_list.indexOf(temp)>=0)
+        {
+            list.removeOne(temp);
+        }
+    }
+    this->clear();
+    this->addItems(m_list);
+}
+
+void MyListWidget::mousePressEvent(QMouseEvent *event)
+{
+    //qDebug()<<"mousePressEvent"<<event;
+}
+
+void MyListWidget::mouseMoveEvent(QMouseEvent *event)
+{
+    //qDebug()<<"mouseMoveEvent"<<event;
+}
+
+void MyListWidget::dragEnterEvent(QDragEnterEvent *event)
+{
+    QList<QUrl> urls = event->mimeData()->urls();
+    foreach (QUrl u, urls) {
+        if(QFileInfo(u.toLocalFile()).isFile())
+        {
+            event->acceptProposedAction();
+            return;
+        }
+    }
+}
+
+void MyListWidget::dragMoveEvent(QDragMoveEvent *event)
+{
+    //qDebug()<<"dragMoveEvent"<<event;
+}
+
+void MyListWidget::dropEvent(QDropEvent *event)
+{
+    //qDebug()<<"dropEvent"<<event;
+    QList<QUrl> urls = event->mimeData()->urls();
+    if(urls.isEmpty())
+    {
+        return;
+    }
+    foreach (QUrl u, urls) {
+        QString filepath = u.toLocalFile();
+        if(QFileInfo(filepath).isFile())
+        {
+            this->addItem(filepath);
+        }
+    }
+}
--- source/widget/mylistwidget.h
+++ source/widget/mylistwidget.h
@@ -4,15 +4,32 @@
 #include <QObject>
 #include <QWidget>
 #include <QListWidget>
+#include <QDebug>
+#include <QDragEnterEvent>
+#include <QUrl>
+#include <QMimeData>
+#include <QFileInfo>
 
 class MyListWidget : public QListWidget
 {
     Q_OBJECT
 public:
-    explicit MyListWidget(QListWidget *parent = 0);
+    explicit MyListWidget(QWidget *parent = 0);
+    void addItem(QString);
+    void addItems(QStringList);
+private:
 
 
+    //重写下面两个函数来完成从该部件中拖拽出去的操作.
+    void mousePressEvent(QMouseEvent *event);
+    void mouseMoveEvent(QMouseEvent *event);
+    //重写下面三个函数来完成接受外来拖拽的操作.
+    void dragEnterEvent(QDragEnterEvent *event);
+    void dragMoveEvent(QDragMoveEvent *event);
+    void dropEvent(QDropEvent *event);
 
+private:
+    QStringList m_list;
 };
 
 #endif // MYLISTWIDGET_H
